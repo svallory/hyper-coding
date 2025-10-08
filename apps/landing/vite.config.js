36f9af5ca892d0aca161bff497557bd49524952f
Saulo Vallory <me@saulo.engineer>
2025-10-08 00:14:05 -0300

    chore: setup netlify deploy
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

---
+++ apps/landing/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import marko from "@marko/run/vite";
+import netlifyAdapter from "@marko/run-adapter-netlify";
+
+export default defineConfig({
+  plugins: [
+    marko({
+      adapter: netlifyAdapter({ edge: true }),
+    }),
+  ],
+});
